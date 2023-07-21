8aa73891d957ced0e3750fcbc0f0f12fb9ac4cc0
Aaron Chen <aaron@moderntreasury.com>
2023-07-20 20:44:20 -0700

    mobile

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <div className="py-4 border-x-0 border-solid border-b mb-6">
-      <div className="flex flex-row gap-4 justify-center">
+    <div className="py-4 border-x-0 border-solid border-b mb-6 bg-gray-50">
+      <div className="flex flex-col gap-4 justify-center items-center md:flex-row">
         <div>
           <p>
-            <Link href="/">SUMMER 2023</Link>
+            <Link href="/">HOME</Link>
           </p>
         </div>
         <div>
