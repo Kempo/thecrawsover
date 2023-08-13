237b96a96e940f862cb2e82e4cb6fe3a12550a6d
Aaron Chen <aaron@moderntreasury.com>
2023-08-12 22:48:23 -0700

    Update

--- components/Current.tsx
+++ components/Current.tsx
@@ -6,8 +6,17 @@ export default function Current() {
         <iframe
           width="100%"
           height="315"
+          src="https://www.youtube.com/embed/3PQhaXctASU"
+          title="Jonathan Kuminga 2023"
+          frameBorder={0}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+        <iframe
+          width="100%"
+          height="315"
           src="https://www.youtube.com/embed/6lLqs8a2wgM"
-          title="YouTube video player"
+          title="Marjon Beauchamp 2023"
           frameBorder={0}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
